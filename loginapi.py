a074b432ae61e870a5b71aa4472dddcc2526b3ba
VZSKY <talay@10ninox.com>
2018-10-23 14:21:46 +0700

    adding anc api

--- loginapi.py
+++ loginapi.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, jsonify
 from flaskext.mysql import MySQL
 import jwt
-from config import *
 
 app = Flask(__name__)
-
 mysql = MySQL()
 
+from config import *
+
 #### APP MYSQL CONFIG
+
 
 mysql.init_app(app)
 
@@ -21,15 +22,18 @@ def auth(usr,pwd):
 	if data is None:
 		return jsonify({"status" : "denied"})
 	token = jwt.encode({"user":usr}, app.config['SECRET_KEY'])
-	return jsonify({"token" : token.decode("utf-8") })
+	return jsonify({"status" : "accepted" , "token" : token.decode("utf-8") })
 
 @app.route('/api/login', methods=['POST'])
 def login():
 	#AUTHEN TOKEN - NO TIMEOUT
-	data = request.json
-	usr = data["user"]
-	pwd = data["pass"]
-	token = auth(usr,pwd)
+	try :
+		data = request.json
+		usr = data["user"]
+		pwd = data["pass"]
+		token = auth(usr,pwd)
+	except : 
+		token = auth(usr,pwd)
 	return token
 
 @app.route('/api/token', methods=['POST'])
@@ -37,29 +41,44 @@ def tkauth():
 	try:
 		#GATHER ALL DATA + CHECK TOKEN
 		token = request.headers['token']
-		print(1);
 		send = jwt.decode(token, app.config['SECRET_KEY'])
-		print(2);
 		user = send["user"]
 		connection = mysql.connect()
 		cursor = connection.cursor()
-		print(3);
 		cursor.execute("SELECT * FROM User WHERE username='" + user + "'")
-		print(45)
 		data = cursor.fetchone()
-		print('')
 		send["first"] = data[2] #gather Firstname
 		send["last"] = data[3]	#gather Lastname
-		print(4);
 		cursor.execute("SELECT * FROM announce")
 		anc = cursor.fetchall()
-		print(5);
 		cursor.close()
 		connection.close()
-		print(6);
 	except:
 		return jsonify({"error":"authen error"})
 	return jsonify({"user":send,"announce":anc})
+
+@app.route('/api/addanc', methods=['POST'])
+def addanc():
+	try :
+		data = request.json
+		top = data["topic"]
+		con = data["content"]
+		connection = mysql.connect()
+		cursor = connection.cursor()
+		cursor.execute("SELECT MAX(id) from announce")
+		maxid = cursor.fetchone()
+		try : 
+			postid = maxid[0] + 1
+		except : 
+			postid = 0
+		cursor.execute("INSERT INTO announce (id,topic,content) VALUES (%s,%s,%s)",(postid, top, con))
+		connection.commit()
+		status = "POSTed"
+		cursor.close()
+		connection.close()
+	except :
+		status = "error"
+	return status
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=7000, debug=True)
